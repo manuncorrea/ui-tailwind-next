4f5210e1cd1d29c9810223374d5a1c4f2aa85b56
Emanuele Correa <emanuelle@taqe.com.br>
2023-03-26 20:09:12 -0300

    masterclas ignite utilzando tailwind e next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
+import { Header } from '@/components/Header'
+import { HeroPattern } from '@/components/HeroPattern'
+import { Sidebar } from '@/components/Sidebar'
+import { Inter } from 'next/font/google'
 import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,8 +18,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body className='bg-zinc-900'>
+        <Sidebar />
+
+        <div className='ml-80 relative h-screen'>
+          <Header />
+          <HeroPattern />
+          <div className='py-24 max-w-4xl px-8 mx-auto'>
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
